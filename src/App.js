cb1d6b4ee1e328248778f22c68e575bc770f947a
Bob Hunt <bob.hunt.email@gmail.com>
2018-08-13 10:32:44 -0400

    items left is not working. shows the same number as items total.

--- src/App.js
+++ src/App.js
@@ -6,13 +6,23 @@ import todoList from './todos.json';
 const TodoItem = (props) => {
   return (
     <div>
-        <li className="">
+        {/* // replaced this code  */}
+        {/* <li className="">
             <div className="view">
                 <input className="toggle" type="checkbox" />
                     <label>{props.title}</label>
                     <button className="destroy"></button>
             </div>
+        </li> */}
+        {/* // with this code  */}
+        <li className={props.todo.completed ? "completed" : ""}>
+            <div className="view">
+                <input className="toggle"  onChange={props.todoToggleClick(props.todo.id)} type="checkbox" checked={props.todo.completed}/>
+                    <label>{props.todo.title}</label>
+                    <button className="destroy" onClick={props.todoDestroy(props.todo.id)}></button>
+            </div>
         </li>
+
     </div>
   )     
 }
@@ -23,7 +33,11 @@ class TodoLists extends Component {
     return (
       <div>
         <ul className="todo-list">
-          {this.props.todos.map( todo => <li><TodoItem title={todo.title}  /></li> )}
+          {/* // replaced this code  */}
+          {/* {this.props.todos.map( todo => <li><TodoItem title={todo.title}  /></li> )} */}
+          {/* // with this code  */}
+          {this.props.todos.map( todo => <TodoItem key={todo.id} todo={todo} todoDestroy={this.props.todoDestroy} todoToggleClick={this.props.todoToggleClick}/> )}
+
         </ul>
       </div>
       );
@@ -33,8 +47,69 @@ class TodoLists extends Component {
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { todos: todoList }
+    this.state = { 
+      todos: todoList,
+      // added input to state
+      input: "", }
   }
+
+  // added todoToggleClick
+  todoToggleClick = (id) => (event) => {
+    this.setState({
+      todos: this.state.todos.map(
+        todo => todo.id === id ? {
+          ...todo, 
+          completed: !todo.completed 
+        } : { 
+          ...todo
+        }
+      )
+    })
+  }
+
+  // added todoDestroy
+  todoDestroy = (id) => (event) => {
+    this.setState({
+      todos: this.state.todos.filter(
+        todo => todo.id !== id 
+      )
+    })
+    console.log("todoDestroy button was clicked")
+    console.log(this.state.todos)
+  }
+
+  // added clearCompleted
+  clearCompleted = (onClick) => {
+    this.setState({
+      todos: this.state.todos.filter(
+        todo => todo.completed !== true 
+      )
+    })
+    console.log("clearCompleted button was clicked")
+    console.log(this.state.todos)
+  }
+
+  // added handleChange
+  handleChange = (event) => {
+    this.setState({ input: event.target.value });
+  }
+
+  // added handleSubmit
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.setState({
+      todos: [...this.state.todos, 
+        {
+        title: this.state.input,
+        id: this.state.todos[this.state.todos.length-1].id+1,
+        completed: false
+        },
+      ],
+      input: ""
+    });
+    console.log(this.state.todos);
+  }
+
 
   render() {
     console.log(todoList)
@@ -45,18 +120,39 @@ class App extends Component {
       <section className="todoapp">
         <header className="header">
           <h1>todos</h1>
-          <input className="new-todo" placeholder="What needs to be done?" autoFocus />
+          {/* // replaced this code  */}
+          {/* <input className="new-todo" placeholder="What needs to be done?" autoFocus /> */}
+          {/* // with this code */}
+          <form onSubmit={this.handleSubmit}>
+            <input value={this.state.input} onChange={this.handleChange} className="new-todo" placeholder="What needs to be done?" autoFocus />
+          </form>
+
         </header>
 
+
         <section className="main">
-        <TodoLists todos={this.state.todos} />
+          {/* // replaced this code  */}
+          {/* <TodoLists todos={this.state.todos} /> */}
+          {/* // with this code  */}
+          <TodoLists todoDestroy={this.todoDestroy} todoToggleClick={this.todoToggleClick} todos={this.state.todos} />
+
         </section>
 
         <footer className="footer">
+          {/* // replaced this code  */}
+          {/* <span className="todo-count"><strong>0</strong> item(s) left</span> */}
+          {/* // with this code  */}
+          <span className="todo-count"><strong>{this.state.todos.length}</strong> item(s) total</span>
+          <span className="space"><strong> </strong></span>
+          <span className="total-count"><strong>{this.state.todos.length}</strong> item(s) left</span>
 
-          <span className="todo-count"><strong>0</strong> item(s) left</span>
+          {/* // replaced this code  */}
+          {/* <button className="clear-completed">Clear completed</button> */}
+          {/* // with this code  */}
+          <button className="clear-completed" onClick={this.clearCompleted} >
+            Clear completed
+          </button>
 
-          <button className="clear-completed">Clear completed</button>
         </footer>
       </section>
   
